b1b86bf56a2653d643630cf326e53873aabb0feb
Malak-Amr-Elsadek <malakelsadek.2015@gmail.com>
2024-11-10 15:40:15 +0300

    Not equal operator

---
+++ main.py
@@ -0,0 +1,22 @@
+a=10
+b=15
+c=10
+
+print(a!=b)
+print(a!=c)
+
+
+
+a="python"
+b="Malak"
+
+if a!=b:
+    print("Not equal!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+
+
+a=4
+b=5
+
+if (a==1)!=(b==5):
+    print("Hello!!! | Ola!!! | Bonjour!!!")
